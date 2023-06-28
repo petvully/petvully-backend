2c1d2740d0708a3e654708dfb47e7fb2b79b73f9
Pyosangwoo <psw3619@naver.com>
2023-06-29 00:35:02 +0900

    init setting

---
+++ src/main/java/com/hackathon/petvully/PetvullyApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.petvully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetvullyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetvullyApplication.class, args);
+	}
+
+}
