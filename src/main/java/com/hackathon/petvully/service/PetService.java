9180f639c6d609a9b491c59ea249175e4b7a9aab
hyunseo Jeong <wjdgustj2877@naver.com>
2023-06-29 13:01:24 +0900

    define entity

--- src/main/java/com/hackathon/petvully/service/PetService.java
+++ src/main/java/com/hackathon/petvully/service/PetService.java
@@ -1,9 +1,22 @@
 package com.hackathon.petvully.service;
 
+import com.hackathon.petvully.entity.Pet;
+import com.hackathon.petvully.repository.PetRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class PetService {
+    private final PetRepository petRepository;
+
+    @Transactional
+    public Pet findById(long id) {
+        Optional<Pet> foundPet = petRepository.findById(id);
+        return foundPet.orElse(null);
+    }
 }
