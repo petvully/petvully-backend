6578b9a04bf4a78d2e6875aa84d1397b01b677a8
Pyosangwoo <psw3619@naver.com>
2023-06-29 09:49:11 +0900

    feat. Eamil Dup

--- src/main/java/com/hackathon/petvully/controller/UserController.java
+++ src/main/java/com/hackathon/petvully/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.hackathon.petvully.controller;
 
+import com.hackathon.petvully.dto.UserDTO.EmailDupDTO;
 import com.hackathon.petvully.dto.UserDTO.SignUpDTO;
+import com.hackathon.petvully.entity.User;
 import com.hackathon.petvully.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,10 +22,19 @@ public class UserController {
     @Operation(summary = "", description = "회원가입 API")
     @PostMapping("/signup")
     public ResponseEntity<String> join(@RequestBody SignUpDTO signUpDTO) {
-        if(userService.isEmailDuplicate(signUpDTO.getEmail())) {
+        if (userService.isEmailDuplicate(signUpDTO.getEmail())) {
             return ResponseEntity.status(400).body("Email already exists");
         }
         userService.join(signUpDTO);
         return ResponseEntity.ok("Join Success!");
+    }
+
+    @Operation(summary = "", description = "이메일 중복 확인 API")
+    @PostMapping("/emailDup")
+    public ResponseEntity<String> emailDup(@RequestBody EmailDupDTO email) {
+        if (userService.isEmailDuplicate(email.getEmail())) {
+            return ResponseEntity.status(400).body("Email already exists");
+        }
+        return ResponseEntity.ok("You can signup");
     }
 }
